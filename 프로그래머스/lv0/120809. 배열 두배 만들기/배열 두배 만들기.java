5978170418a8305f4471f92fbde2b017203a39ae
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2023-06-06 14:37:11 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+            int i=0;
+            for (i=0; i<numbers.length; i++){
+                answer[i] = numbers[i]*2;
+            }          return answer;
+        }
+
+    }
