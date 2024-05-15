94452a0f9ecb5c7731a15bef80c9b589cde14834
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2024-05-15 10:58:05 +0900

    [level 2] Title: 점 찍기, Time: 0.09 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public long solution(int k, int d) {
+       long answer = 0;
+
+        for (int x = 0; x <= d; x += k) {
+            long maxY = (long) Math.sqrt((long) d * d - (long) x * x);
+            answer += (maxY / k) + 1;
+        }
+
+        return answer;
+    
+    }
+}
