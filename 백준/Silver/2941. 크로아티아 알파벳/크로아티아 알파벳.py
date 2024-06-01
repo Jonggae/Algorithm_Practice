564ffd884af96129bff34b0377e86ca97d67b107
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2024-06-01 19:55:15 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,9 +1,10 @@
 word = input()
 
 croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+# 크로아티아 문자를 발견하면 *로 치환 후 마지막 길이 측정
+# 변경은 한번에 하나씩 
 
 length = len(word)
-for c in croatia:
-   while c in word:
-       word = word.replace(c,"*",1)
+for c in croatia:   
+    word = word.replace(c,"*")
 print(len(word))
