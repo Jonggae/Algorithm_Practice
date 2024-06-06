3c32ad9ba96ffd4b1f6d4caf1b01d8d5bf79cfc8
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2024-06-06 15:33:07 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,22 @@
+
+n = int(input())
+letters = list(input())
+
+
+def char_to_num(char):
+    return ord(char) - 96
+
+
+def find_hash(nums):
+    r = 31
+    hash = 0
+    for i in range(len(nums)):
+        hash += nums[i] * r**i
+    return hash
+
+nums = []
+
+for char in letters:
+    nums.append(char_to_num(char))
+
+print(find_hash(nums))
