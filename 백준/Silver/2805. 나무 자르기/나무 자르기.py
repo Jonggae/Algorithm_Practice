862ada0e05881e7060a6be59e19c8b5a9762616d
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2024-06-07 15:10:19 +0900

    [Silver II] Title: 나무 자르기, Time: 2232 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,27 @@
+n, m = map(int, (input().split()))
+trees = list(map(int, input().split()))
+
+
+def wood_cut(trees, height):# 해당 톱으로 자른 나무 길이의 합
+    wood_h = 0
+    for tree in trees:
+        if tree > height:
+            wood_h += tree-height
+    return wood_h
+
+def saw_height(trees, m):
+    low, high = 0, max(trees)
+    result = 0
+
+    while low <= high:
+        mid = (low + high) // 2
+        if wood_cut(trees, mid) >= m:
+            result = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return result
+
+
+print(saw_height(trees, m))
