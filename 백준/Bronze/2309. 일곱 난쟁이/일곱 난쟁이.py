83270320eeabd76f8086e8c4896536fb787d2b08
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2024-06-07 09:05:05 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,19 @@
+from itertools import *
+
+height = list(int(input()) for _ in range(9))
+sum_height = sum(height)
+
+
+# 조합? 으로 7개를 선택하여 100이되는 경우를 찾아본다?
+
+answer = list(permutations(height, 2))
+
+for c in answer:
+    if sum(c) == sum_height - 100:
+        height.remove(c[0])
+        height.remove(c[1])
+        
+        break
+ans = sorted(height)
+for i in ans:
+    print(i)
