f5f17b29e6af5910189f4e8f944ebc8eb3946f87
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2024-06-08 19:36:16 +0900

    [Bronze II] Title: 음계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+scale = list(map(int, (input().split())))
+
+if scale == [1,2,3,4,5,6,7,8]:
+    print("ascending")
+elif scale == [8,7,6,5,4,3,2,1]:
+    print("descending")
+else:
+    print("mixed")
